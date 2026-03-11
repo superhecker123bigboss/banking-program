d0b5b307aa36dca5f8e3a3d41a42fb4c2dc8f11a
superhecker123bigboss <zanden2011@gmail.com>
2026-03-11 19:20:53 +0100

    Refactor bank application to include account class
    I added a bit of OOP in my program. ive not been very active but i want to increase my pace and implement pointers in this program in around 1 - 2 weeks

--- project.cpp
+++ project.cpp
@@ -5,21 +5,159 @@ void deposit(float& totalBalance);
 void withdraw(float& totalBalance);
 void totalAmount(float& totalBalance);
 
+class account{
+    private:
+    std::string password = "1";
+    std::string name = "1";
 
+    public:
+    void setPassword(){
+        if(password == "1"){
+            std::string newPassword;
+            std::string choice;
+
+            std::cout << "enter the password that you want to use" << std::endl;
+            std::cin >> newPassword; 
+
+            std::cout << "are you sure that you want your password to be: " << newPassword << std::endl;
+            std::cout << "awnser with a YES or NO " << std::endl;
+            std::cin >> choice;
+
+            if(choice == "YES" || choice == "Yes" || choice == "yes"){
+                std::cout << "your new password has been confirmed" << std::endl;
+                password = newPassword;
+            } else if(choice == "NO" || choice == "No" || choice == "no"){
+                std::cout << "password has been canceled" << std::endl;
+            } else{
+                std::cout << "invalid input, try again" << std::endl;
+            }
+        } else{
+            std::cout << "you have already set a password" << std::endl;
+        }
+    }
+    
+    void setName(){
+    if(name == "1"){
+            std::string newName;
+            std::string choice;
+
+            std::cout << "enter the username that you want to use" << std::endl;
+            std::cin >> newName; 
+
+            std::cout << "are you sure that you want your username to be: " << newName << std::endl;
+            std::cout << "awnser with a YES or NO " << std::endl;
+            std::cin >> choice;
+
+            if(choice == "YES" || choice == "Yes" || choice == "yes"){
+                std::cout << "your new username has been confirmed" << std::endl;
+                name = newName;
+            } else if(choice == "NO" || choice == "No" || choice == "no"){
+                std::cout << "Username has been canceled" << std::endl;
+            } else{
+                std::cout << "invalid input, try again" << std::endl;
+            }
+        } else{
+            std::cout << "you have already set a username" << std::endl;
+        }
+
+    }
+    
+    void changePassword(){ 
+        std::string checkPassword;
+        std::string newPassword;
+        std::string choice;
+        std::cout << "do you want to change your password?" << std::endl;
+        std::cout << "awnser with a YES or NO" << std::endl;
+        std::cin >> choice;
+
+        if(choice == "YES" || choice == "Yes" || choice == "yes"){
+                std::cout << "Enter your password" << std::endl;
+                std::cin >> checkPassword;
+             
+                        if(checkPassword == password){
+                        std::cout << "enter your new password" << std::endl;
+                        std::cin >> newPassword;
+                        password = newPassword;
+                        std::cout << "your password has succesfully been changed to: " << password << std::endl;
+                        } else {
+                        std::cout << "invalid password, try again " << std::endl; 
+                        }
+
+        } else if(choice == "NO" || choice == "No" || choice == "no"){
+                std::cout << "password change request has been canceled" << std::endl;
+        } else{
+                std::cout << "invalid input, try again" << std::endl;
+        }
+    }
+
+        void changeName(){
+        std::string checkPassword;
+        std::string newName;
+        std::string choice;
+        std::cout << "do you want to change your name?" << std::endl;
+        std::cout << "awnser with a YES or NO" << std::endl;
+        std::cin >> choice;
+
+        if(choice == "YES" || choice == "Yes" || choice == "yes"){
+                std::cout << "Enter your password" << std::endl;
+                std::cin >> checkPassword;
+             
+                        if(checkPassword == password){
+                        std::cout << "enter your new username" << std::endl;
+                        std::cin >> newName;
+                        name = newName;
+                        std::cout << "your name has succesfully been changed to: " << name << std::endl;
+                        } else {
+                        std::cout << "invalid password, try again " << std::endl; 
+                        }
+
+        } else if(choice == "NO" || choice == "No" || choice == "no"){
+                std::cout << "Username change request has been canceled" << std::endl;
+        } else{
+                std::cout << "invalid input, try again" << std::endl;
+        }
+    }
+
+    void getName(){
+        std::string checkPassword;
+        std::string choice;
+        std::cout << "do you want to view your name?" << std::endl;
+        std::cout << "awnser with a YES or NO" << std::endl;
+        std::cin >> choice;
+
+        if(choice == "YES" || choice == "Yes" || choice == "yes"){
+                std::cout << "Enter your password" << std::endl;
+                std::cin >> checkPassword;
+             
+                        if(checkPassword == password){
+                        std::cout << "your name is: " << name << std::endl;
+                        } else {
+                        std::cout << "invalid password, try again " << std::endl; 
+                        }
+
+        } else if(choice == "NO" || choice == "No" || choice == "no"){
+                std::cout << "Username view request has been canceled" << std::endl;
+        } else{
+                std::cout << "invalid input, try again" << std::endl;
+        }
+    }
+
+};
 
 
 int main(){
-
+    account myAcc;
+    
+    
     int choice;
     float totalBalance = 0;
-
-    
     
     std::cout << "welcome to the National online bank service of the Netherlands" << std::endl;
     
    do {
     std::cout << "Please enter the number thats corrisponding to what you want to do" << std::endl;
-    std::cout << "1. show total balance ; 2. deposit ; 3. withdraw ; 4. exit program" << std::endl; 
+    std::cout << "1. show total balance ; 2. deposit ; 3. withdraw ; 4. set password ; 5. set name " << std::endl; 
+    std::cout << "6. change password ; 7. change name ; 8. get name ; 9. exit program" << std::endl;
     std::cin >> choice;
 
     switch(choice){
@@ -33,15 +171,28 @@ int main(){
             withdraw(totalBalance);
             break;
         case 4:
+            myAcc.setPassword();
+            break;
+        case 5:
+            myAcc.setName();
+            break;
+        case 6:
+            myAcc.changePassword();
+            break;
+        case 7: 
+            myAcc.changeName();
+            break;
+        case 8: 
+            myAcc.getName();
+            break;
+        case 9:
             std::cout << "Exiting program..." << std::endl;
             break;
         default: 
-            std::cout << "invalid input, Please enter number 1,2,3 or 4" << std::endl;
+            std::cout << "invalid input, Please enter number 1,2,3,4,5,6,7,8 or 9" << std::endl;
 
     }
-    } while(choice != 4); 
-
-
+    } while(choice != 9); 
 
 
  return 0;
@@ -108,5 +259,4 @@ void deposit(float& totalBalance){
         else{
             std::cout << "Invalid user input: Awnser in a yes or no" << std::endl;
         }
-
 }
